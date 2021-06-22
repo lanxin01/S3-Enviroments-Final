f6c54d60fb8d7d7c12568510806959acba11dea6
lanxin01 <73389290+lanxin01@users.noreply.github.com>
2021-06-22 15:24:16 +0100

    Add files via upload

---
+++ Unity/Assets/DemoScenes/跳转.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class 跳转 : MonoBehaviour
+{
+public int 跳转页面;
+    public void OnLoginButtonClick()
+    {
+        SceneManager.LoadScene(跳转页面);
+    }
+}
